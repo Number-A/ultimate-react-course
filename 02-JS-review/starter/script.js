8a67b3663f6095a567e305cfa7cc08f72d822f0e
Number_A <68718087+Number-A@users.noreply.github.com>
2023-10-10 19:57:45 -0400

    Day 1

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,175 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// Destructuring
+
+const book = getBook(3);
+// const title = book.title;
+// const author = book.author;
+// title;
+// author;
+
+/*
+const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
+  book;
+
+console.log(title, author, genres);
+
+// const primaryGenre = genres[0];
+// const secondaryGenres = genres[1];
+//Rest Operator adds the array as the 3rd elements of genres
+const [primaryGenre, secondaryGenres, ...otherGenres] = genres;
+console.log(primaryGenre, secondaryGenres, otherGenres);
+
+//Spread operator adds eleemnts of the array into the new array (can be added in front of back)
+const newGenres = ["epic fantasy", ...genres];
+newGenres;
+
+//Takes properties of book to add in the updated and add a publication date and update the number of pages in the book
+const updatedBook = {
+  ...book,
+  moviePublicationDate: "2001-12-19",
+  pages: 12000,
+};
+updatedBook;
+
+// Arrow functions - One Line Functions
+
+// function getYear(str) {
+//   return str.split("-")[0];
+// }
+const getYear = (str) => str.split("-")[0];
+console.log(getYear(publicationDate));
+
+//Template literals: Any javascript statement works in the curly brackets
+const summary = `${title} a ${pages}-page long book, was written by ${author} and published in ${getYear(
+  publicationDate
+)}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted.`;
+summary;
+
+//Ternaries instead of if/else
+const pagesRange = pages > 1000 ? "over 1000" : "less than 1000";
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
+
+// Short-Circuiting and Logical Operators: &&, ||, ??
+console.log(true && "Some string");
+console.log(false && "Some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+// Falsey values: 0, '', null, undefined
+console.log("jonas" && "Some string");
+console.log(0 && "Some string");
+
+console.log(true || "Some string");
+console.log(false || "Some string");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
+// This code with book 1-2
+// console.log(book.reviews.librarything.reviewsCount);
+// const countWrong = book.reviews.librarything.reviewsCount || "no data";
+// countWrong;
+
+// const count1 = null ?? "no data";
+// count1;
+// const count2 = book.reviews.librarything.reviewsCount ?? "no data";
+// count2;
+
+*/
+
+/*
+// Optional Chaining
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews?.goodreads?.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+  librarything;
+
+  return goodreads + librarything;
+}
+console.log(getTotalReviewCount(book));
+
+// Array map method
+
+const books = getBooks();
+
+const x = [1, 2, 3, 4, 5].map((el) => el * 2);
+console.log(x);
+
+const titles = books.map((book) => book.title);
+titles;
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewsCount: getTotalReviewCount(book),
+}));
+essentialData;
+
+//Array Filter methods
+const longBooks = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooks;
+
+// Array Reduce method This adds a counter essentially
+const pagesAllBooks = books.reduce((acc, book) => acc + book.pages, 0);
+pagesAllBooks;
+
+// Array Sort method
+const arr = [3, 7, 1, 9, 6];
+const asc = arr.sort((a, b) => a - b);
+asc;
+arr;
+const desc = arr.slice().sort((a, b) => b - a); // Slice so that we sort a copied array and not the original
+desc;
+arr;
+
+const sortedByPages = books.slice().sort((a, b) => a.pages - b.pages);
+sortedByPages;
+
+// Immutable Arrays (Tuples)
+
+// 1) Add book object to Array
+const newBook = {
+  is: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J.K. Rowling",
+};
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2) Delete a book Object from Array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id != 3);
+booksAfterDelete;
+
+// 3) Update a book object while in the array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id == 1 ? { ...book, pages: 1 } : book
+);
+booksAfterUpdate;
+*/
+
+/*
+//Asynchronous JS
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+// Essentially this request is sent and while it waits, we say to JS to keep going
+// until we receive the response
+// That is why in the console Jonas is printed before the response
+console.log("jonas");
+*/
+
+// Async/Await - Better syntax then above to have async comparmentalized
+async function getToDos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+getToDos();
+
+console.log("jonas");
